3eeadd00d1f9f59032b9f99711ac3c8dc0d99d23
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-07-25 15:40:00 -0400

    added ejs and a list post method

--- app.js
+++ app.js
@@ -3,8 +3,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello");
+var items = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  });
+});
+
+app.post("/", (req, res) =>{
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 
