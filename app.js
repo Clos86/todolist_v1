004b00f19aba91c43ff0c237acd09abbf8e0d7bb
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-07-25 16:22:11 -0400

    cleaning up code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+let items = [];
 
 app.set("view engine", "ejs");
 
@@ -11,14 +11,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
     kindOfDay: day,
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) =>{
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 
   items.push(item);
 
