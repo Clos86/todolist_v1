f633c91dc84d38474be0ac38a90b8c5a44528560
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-09-17 13:24:43 -0400

    refactoring code

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -12,29 +14,36 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+const day = date.getDate();
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
 
 app.post("/", (req, res) =>{
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
-  items.push(item);
-
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
+app.get("/work", (req, res) =>{
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
 app.listen(3000, () => {
   console.log("Serrver running on port 3000");
